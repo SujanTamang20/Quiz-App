485c07ecff8f6030a24e9ee5f7479f8cfdca1c1d
Sujan Tamang <162395480+SujanTamang20@users.noreply.github.com>
2024-06-02 17:21:02 +0900

    Update script.js

--- script.js
+++ script.js
@@ -150,7 +150,7 @@ function showResult(){
     quiz_box.classList.remove("activeQuiz"); //hide quiz box
     result_box.classList.add("activeResult"); //show result box
     const scoreText = result_box.querySelector(".score_text");
-    if (userScore > 3){ // if user scored more than 6
+    if (userScore > 6){ // if user scored more than 6
         //creating a new span tag and passing the user score number and total question number
         let scoreTag = '<span>and congrats! 🎉, You got <p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>';
         scoreText.innerHTML = scoreTag;  //adding new span tag inside score_Text
