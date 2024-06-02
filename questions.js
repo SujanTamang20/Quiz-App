cd53cabbdd663f5ee66f8c062159d2a2d21eb6d7
Sujan Tamang <162395480+SujanTamang20@users.noreply.github.com>
2024-06-02 17:09:26 +0900

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,114 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What's the main job of the CPU in a computer?",
+    answer: "Arithmetic and logic operations",
+    options: [
+      "Data storage",
+      "Arithmetic and logic operations",
+      "Graphics display",
+      "Internet connectivity"
+    ]
+  },
+    {
+    numb: 3,
+    question: " Which property is used to change the background color in CSS?",
+    answer: "background-color",
+    options: [
+      "background-color",
+      "bg-color",
+      "color-background",
+      "color-bg"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which is a graphical user interface (GUI) OS?",
+    answer: "Windows",
+    options: [
+      "Windows",
+      "Linux",
+      "macOS",
+      "Unix"
+    ]
+  },
+  
+  {
+   numb: 6,
+  question: " Commonly used language for web development and scripting?",
+    answer: "Python",
+  options: [
+       "Java",
+       "Swift",
+       "C++",
+       "Python"
+    ]
+  },
+  {
+    numb: 7,
+   question: " What does TCP/IP stand for in networking?",
+     answer: "Transmission Control Protocol/Internet Protocol",
+   options: [
+        "Transmission Control Protocol/Internet Protocol",
+        "Time Cycle Protocol/Internet Process",
+        "Transfer Command Process/Internet Protocol",
+        "Total Control Power/Internet Port"
+     ]
+   },
+   {
+    numb: 8,
+   question: "Which stores permanent data even when the power is off?",
+     answer: "Hard Drive (HDD) or Solid State Drive (SSD)",
+   options: [
+        "CPU",
+        "RAM",
+        "Hard Drive (HDD) or Solid State Drive (SSD)",
+        "GPU"
+     ]
+   },
+   {
+    numb: 9,
+   question: "What device is used to connect multiple computers in a local area network (LAN)?",
+     answer: "Switch",
+   options: [
+        "Switch",
+        "Router",
+        "Modem",
+        "Firewall"
+     ]
+   },
+   {
+    numb: 10,
+   question: "Which type of testing evaluates the functionality of a software system as a whole?",
+     answer: "Integration testing",
+   options: [
+        "Unit testing",
+        "Regression testing",
+        "Integration testing",
+        "User acceptance testing"
+     ]
+   },
+];
